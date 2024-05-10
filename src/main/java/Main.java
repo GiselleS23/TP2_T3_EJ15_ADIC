5e53090aed07b44b50e7e7015099705e6239ab56
GiselleS23 <128999381+GiselleS23@users.noreply.github.com>
2024-05-10 00:50:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+/* Escriba un diagrama que muestre la tabla ASCII.
+Sacchetti, Maria Giselle C2*/
+
+/* 32 (el primer carácter imprimible en la tabla ASCII) y terminamos en 126 (el último carácter imprimible en la tabla ASCII). */
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    for (int i = 32; i <= 126; i++) {
+        System.out.printf("%-5d%-5c\n", i, (char) i);
+    }
+    scanner.close();
+
+  }
+
+}
